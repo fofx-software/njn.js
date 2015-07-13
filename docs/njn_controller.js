340ad04778847a577a51ca4560d2502458a21e2b
Joseph Swetnam <jbswetnam@gmail.com>
2015-07-12 21:26:12 -0400

    total refactor using outerHTML extensively for simpler code

--- docs/njn_controller.js
+++ docs/njn_controller.js
@@ -6,19 +6,16 @@ njn.controller = function(controllerName, viewInterface) {
   var controller = new njn.Controller;
 
   controller.viewInterface = viewInterface || (njn.isObject(controllerName) ? controllerName : {});
+  controller.viewInterface.controller = controller;
 
   if(njn.isString(controllerName)) {
     controller.name = controllerName;
     njn.registerController(controllerName, controller);
-    var template = document.querySelector(controller.liveElement());
+    var template = document.getElementById(controllerName);
     if(template) controller.loadTemplate(template);
   }
 
   return controller;
-}
-
-njn.Controller.prototype.liveElement = function() {
-  return document.querySelector('[data-njn="' + this.name + '"]');
 }
 
 njn.Controller.prototype.loadTemplate = function(template) {
@@ -28,53 +25,89 @@ njn.Controller.prototype.loadTemplate = function(template) {
 }
 
 njn.Controller.prototype.refreshView = function() {
-  this.liveElement().outerHTML = this.processHTML(this.template, this.viewInterface);
-  return this.liveElement();
+  var processed = processHTML(this.template, this.viewInterface);
+  (document.getElementById(this.name) || this.template).outerHTML = processed;
+  repeatElements(document.getElementById(this.name), this.viewInterface, 1);
+  var liveElement = document.getElementById(this.name);
+  liveElement.outerHTML = stripBracketsAndTripleBraces(liveElement.outerHTML);
+  return document.getElementById(this.name);
 }
 
-njn.Controller.prototype.processHTML = function(elementOrHTML, resolveIn) {
+function repeatElements(parentElement, resolveIn, depth) {
+  var plusSigns = '';
+  for(var i = 0; i < depth; i++) { plusSigns += '+'; }
+  var repeaters = parentElement.querySelectorAll('[data-njnrepeat]:not([data-njnrepeat^="' + plusSigns + '"]');
+  for(i = 0; i < repeaters.length; i++) {
+    var repeater = repeaters[i];
+    var list = resolveValue(repeater.dataset.njnrepeat.match(/\w+/)[0], resolveIn);
+    var container = document.createElement('div');
+    for(var j = 0; j < list.length; j++) {
+      container.innerHTML += processHTML(repeater, list[j], depth);
+      repeatElements(container.lastChild, list[j], depth + 1);
+    }
+    repeater.outerHTML = container.innerHTML;
+  }
+}
+
+function processHTML (elementOrHTML, resolveIn, depth) {
   var container = document.createElement('div');
   container.innerHTML = elementOrHTML.outerHTML || elementOrHTML;
-  while(container.innerHTML.search(interpolatorRE) > -1) {
-    container.innerHTML = processText(container.innerHTML, resolveIn);
+  var interpolator = interpolatorRE(depth);
+  while(container.innerHTML.search(interpolator) > -1) {
+    container.innerHTML = processText(container.innerHTML, resolveIn, interpolator);
   }
-  return stripBracketsAndTripleBraces(container.innerHTML);
+  return container.innerHTML;
 }
 
-var interpolatorRE = /\{\{!?\w+\??\}\}(?!\})/g;
-var escapeHTMLRE = /\[\[([^]|\n)+\]\]/g;
+function interpolatorRE(depth) {
+  var plusSigns = '';
+  for(var i = 0; i < depth; i++) {
+    plusSigns += '\\+';
+  }
+  var regexp = '\\{\\{' + plusSigns + '[!=]?\\w+\\??\\}\\}(?!\\})';
+  return new RegExp(regexp, 'g');
+}
 
-function processText(text, resolveIn) {
-  return text.replace(interpolatorRE, function(match) {
-    var negate = /^\{\{!/.test(match);
-    var innerMatch = match.match(/\w+\??/)[0];
-    var replacement = resolveValue(innerMatch, resolveIn);
-    if(negate) { replacement = !replacement; }
-    if(njn.isHTMLElement(replacement)) {
-      replacement = replacement.outerHTML;
+var escapeHTMLRE = /\[\[([^\]]|\n)+\]\]/g;
+
+function processText(text, resolveIn, interpolator) {
+  return text.replace(interpolator, function(match) {
+    var innerMatch = match.match(/\{\{\+*([^\+]+)\}\}/)[1];
+    var negate = /^\{\{!/.test(innerMatch);
+    var propertyName = match.match(/\w+\??/)[0];
+    var replacement = resolveValue(propertyName, resolveIn);
+    if(njn.isDefined(replacement)) {
+      if(negate) { replacement = !replacement; }
+      if(njn.isHTMLElement(replacement)) {
+        replacement = replacement.outerHTML;
+      }
+      return replacement;
+    } else {
+      return match + '}';
     }
-    return replacement;
   }).replace(escapeHTMLRE, function(match) {
     return match.replace(/</g, '&lt;').replace(/>/g, '&gt;');
   });
 }
 
 function resolveValue(propertyName, resolveIn) {
-  var value = resolveIn[propertyName];
-  return value.call ? value.call(resolveIn) : value;
+  var splitProperty = propertyName.split('.');
+  var firstProperty = splitProperty.shift();
+  if(njn.hasProperty(resolveIn, firstProperty)) {
+    var value = resolveIn[firstProperty];
+    value = value.call ? value.call(resolveIn) : value;
+    while(splitProperty.length) {
+      subValue = value[splitProperty.shift()];
+      value = subValue.call ? subValue.call(value) : subValue;
+    }
+    return value;
+  }
 }
 
 function stripBracketsAndTripleBraces(html) {
   return html.replace(/\[?\[\[|\]?\]\]/g, function(match) {
     return match.length === 3 ? match.slice(0,2) : '';
   }).replace(/\{\{\{/g, '{{').replace(/\}\}\}/g, '}}');
-}
-
-njn.Controller.prototype.mapHTML = function(list, elementOrHTML, join) {
-  var mapped = list.map(function(member) {
-    return this.processHTML(elementOrHTML, member);
-  }, this);
-  return njn.isDefined(join) ? mapped.join(join) : mapped;
 }
 
 if(window['testing'])
--- docs/njn_controller/index.js
+++ docs/njn_controller/index.js
@@ -1,34 +1,171 @@
-var Chapter = function() {
-  
+var Chapter = function(title, sections) {
+  this.title = title;
+  this.sections = sections;
 }
 
-var chapters = [{ }];
+Chapter.prototype.id = function() {
+  return this.title.replace(/:|<[^>]+|'>/g, '').replace(/\./g, ' ').toLowerCase().split(' ').join('-');
+}
 
-var leftPanel = njn.controller({
-  chapterLinks: njn.Controller.mapHTML(chapters, chapterLinks),
-  chapter
+var Section = function(title, subsections, sectionBody) {
+  this.title = title;
+  (this.subsections = subsections).forEach(function(subsection, i) {
+    subsection.id = this.id() + '-subsection' + (i + 1);
+  }, this);
+  this.sectionBody = sectionBody || 
+    '<div data-njnrepeat="++subsections">' +
+      '<h3 class="subsection-title">' +
+        '<a class="subsection-number" href="#{{+++id}}" id="{{+++id}}"></a>' +
+        ' {{+++title}}' +
+      '</h3>' +
+      '<div class="subsection-body">{{+++content}}</div>' +
+    '</div>';
+}
+
+Section.prototype = new Chapter;
+
+var SubSection = function(title, content) {
+  this.title = title;
+  this.content = content || '';
+}
+
+/****/
+/****/
+
+var chapter1Section1 = new Section('Download the code', [
+  new SubSection('<a target="_blank" href="http://github.com/fofx-software/njn.js/blob/master/njn.js">njn.js</a>'),
+  new SubSection('<a target="_blank" href="http://github.com/fofx-software/njn.js/blob/master/njn_controller.js">njn_controller.js</a>')
+]);
+
+var chapter1Section2 = new Section('Put it in your HTML', [],
+  '<pre class="language-markup"><code>[[' +
+    '<head>\n' +
+    '  <script src="njn.js"></script> <!-- run this first! -->\n' +
+    '  <script src="njn_controller.js"></script>\n' +
+    '</head>' +
+  ']]</code></pre>' +
+  '<i>syntax highlighting courtesy of <a href="http://prismjs.com" target="_blank">Prism</a></i>'
+);
+
+var chapter1Section3 = new Section('Put it to work', [],
+  '<pre class="language-markup"><code>[[' +
+    '<body>\n' +
+    '  <div id="example-controller">\n' +
+    '    {{exampleText}}} <!-- will resolve to "hello world" -->\n' +
+    '  </div>\n' +
+    '  <script>\n' +
+    '    njn.controller(\'example-controller\', {\n' +
+    '      exampleText: \'hello world\'\n' +
+    '    });\n' +
+    '  </script>\n' +
+    '</body>' +
+  ']]</code></pre>'
+);
+
+var chapter1 = new Chapter('Quick Start', [chapter1Section1, chapter1Section2, chapter1Section3]);
+
+var chapter2Section1 = new Section('Initialize the njn.controller', [
+  new SubSection(
+    'Initialize an instance of njn.Controller:',
+    '<pre class="language-javascript"><code>njn.controller();</code></pre>'
+  ),
+  new SubSection(
+    'The first argument gives the njn.controller a name so you can bind it to an element in your HTML:',
+    '<pre class="language-javascript"><code>njn.controller(\'example-controller\');</code></pre>'
+  ),
+  new SubSection(
+    'The second argument provides the njn.controller\'s viewInterface:',
+    '<pre class="language-javascript"><code>njn.controller(\'example-controller\', {\n' +
+    '  exampleText: \'hello world\'\n' +
+    '});</pre></code>'
+  ),
+  new SubSection(
+    'It\'s also possible to pass only a viewInterface:',
+    '<pre class="language-javascript"><code>njn.controller({\n' +
+    '  exampleText: \'hello world\'\n' +
+    '});</code></pre>'
+  )
+]);
+
+var chapter2Section2 = new Section('Bind the njn.controller to an element', [
+  new SubSection(
+    'If the njn.controller was given a name and an element exists whose id is the same, that element is automatically processed:',
+    '<pre class="language-markup"><code>' +
+      '[[<div id="example-controller"></div>\n<script>njn.controller(\'example-controller\');</script>]]' +
+    '</code></pre>'
+  ),
+  new SubSection(
+    'Otherwise, you can bind an element to it explicitly with loadTemplate():',
+    '<pre class="language-markup"><code>[[' +
+      '<div id="example-controller"></div>\n' +
+      '<script>\n' +
+      '  njn.controller().loadTemplate(document.getElementById(\'example-controller\'));\n' +
+      '</script>' +
+    ']]</code></pre>'
+  )
+]);
+
+var chapter2Section3 = new Section('Access the njn.controller\'s viewInterface', [
+  new SubSection(
+    'The properties of the njn.controller\'s viewInterface can be accessed directly from within the bound element (or its children):',
+    '<pre class="language-markup"><code>[[' +
+      '<div id="example-controller">\n' +
+      '  {{exampleText}} <!--will resolve to "hello world"-->\n' +
+      '</div>\n' +
+      '<script>\n' +
+      '  njn.controller(\'example-controller\', {\n' +
+      '    exampleText: \'hello world!\'\n' +
+      '  });\n' +
+      '</script>' +
+    ']]</code></pre>'
+  ),
+  new SubSection(
+    'If the accessed property is a function, it is called on the viewInterface without any arguments:',
+    '<pre class="language-markup"><code>[[' +
+      '<div id="example-controller">\n' +
+      '  {{getText}} <!--will resolve to "my text"-->\n' +
+      '</div>\n' +
+      '<script>\n' +
+      '  njn.controller(\'example-controller\', {\n' +
+      '    myText: \'my text\',\n' +
+      '    getText: function() {\n' +
+      '      if(!arguments.length) return this.myText;\n' +
+      '    }\n' +
+      '  });\n' +
+      '</script>' +
+    ']]</code></pre>'
+  )
+]);
+
+var chapter2Section4 = new Section('Using Interpolators', [
+  new SubSection(
+    'Basic usage:',
+    '<pre class="language-markup"><code>[[' +
+      '<div id="example-controller">\n' +
+      '  <p class={{className}}>{{textContent}}</p>\n' +
+      '</div>\n' +
+      '<script>\n' +
+      '  njn.controller(\'example-controller\', {\n' +
+      '    className: "example-class",\n' +
+      '    textContent: "hello world"\n' +
+      '  });\n' +
+      '</script>' +
+    ']]</code></pre>' +
+    'Interpolators appear between double braces &mdash; <b>{{propertyReferenceHere}}}</b> &mdash; and they correspond to property names wthin a viewInterface or other applicable object (more on that <a href="">below</a>). They can be used in element attributes or in elements\' textContent, as shown above.'
+  )
+]);
+
+var chapter2 = new Chapter('Basic Usage', [chapter2Section1, chapter2Section2, chapter2Section3, chapter2Section4]);
+
+var chapters = [chapter1, chapter2];
+
+/****/
+/****/
+
+njn.controller('left-panel', {
+  chapters: chapters
 });
 
-var chapterLinks =
-  '<div njn-foreach="chapters">' +
-    '<h2><a njn-href="#{{sectionId}}">{{title}}</a></h2>' +
-    '<div njn-foreach="sections" class="navlink">' +
-      '<a class="link-to-subsection" njn-href="#{{sectionId}}">{{title}}</a>' +
-    '</div>' +
-  '</div>';
-
-leftPanel.loadTemplate(document.getElementById('left-panel'));
-
-var chapterDivs =
-  '<div njn-foreach="chapters" class="chapter" njn-id="{{sectionId}}">' +
-    '<h1 class="section-break">{{title}}</h1>' +
-    '<div class="section-wrapper" njn-id="{{sectionId}}" njn-foreach="sections">' +
-      '<div>' +
-        '<a class="section-number" njn-href="#{{sectionId}}"><h2></h2></a>' +
-        '<h2 class="section-title">{{title}}:</h2>' +
-      '</div>' +
-      '<div class="section-body">' +
-        '{{sectionBody}}' +
-      '</div>' +
-    '</div>' +
-  '</div>';
+njn.controller('main-body', {
+  chapters: chapters
+});
